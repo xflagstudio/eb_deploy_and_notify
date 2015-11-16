b893fa07f8b72f6d177a61e1a2b9dbf5c7735115
Michael H. Oshita <ijinpublic+github@gmail.com>
2015-11-17 00:54:14 +0900

    escape quotes

--- eb_deploy_and_notify.sh
+++ eb_deploy_and_notify.sh
@@ -16,4 +16,4 @@ else
     export SL_ICON="https://www.cloudbees.com/sites/default/files/eleasticbeanstalk_square.png"
 fi
 
-curl -X POST --data-urlencode 'payload={"username": "Elastic Beanstalk", "icon_url": "'"$SL_ICON"'", "channel": "${CHANNEL:-#test}", "attachments": [{ "color": "'"$SL_COLOR"'", "text": "'"$SL_TEXT"'", "mrkdwn_in": ["text"] }] }' https://hooks.slack.com/services/${SLACK_HOOK}
+curl -X POST --data-urlencode 'payload={"username": "Elastic Beanstalk", "icon_url": "'"$SL_ICON"'", "channel": "'"${CHANNEL:-#test}"'", "attachments": [{ "color": "'"$SL_COLOR"'", "text": "'"$SL_TEXT"'", "mrkdwn_in": ["text"] }] }' https://hooks.slack.com/services/${SLACK_HOOK}
