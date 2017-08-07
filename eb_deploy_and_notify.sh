5f94d6fe9510954c09d1fd408c397ccc8f7cde45
Michael H. Oshita <ijinpublic+github@gmail.com>
2017-08-07 14:29:59 +0900

    (un)mute datadog monitor before/after deployment

--- eb_deploy_and_notify.sh
+++ eb_deploy_and_notify.sh
@@ -4,6 +4,22 @@ set -x
 export SHA1=`echo ${CIRCLE_SHA1} | cut -c1-7`
 export ENV=`echo $1 | rev | cut -d \- -f1 | rev`
 
+function dd_mute() {
+    if [[ ${DD_MUTE_ID+x} ]]; then
+        echo "muting DD monitor id: ${DD_MUTE_ID}"
+        curl -X POST "https://app.datadoghq.com/api/v1/monitor/${DD_MUTE_ID}/mute?api_key=${DD_API_KEY}&application_key=${$DD_APP_KEY}"
+    fi
+}
+
+function dd_unmute() {
+    if [[ ${DD_MUTE_ID+x} ]]; then
+        echo "un-muting DD monitor id: ${DD_MUTE_ID}"
+        curl -X POST "https://app.datadoghq.com/api/v1/monitor/${DD_MUTE_ID}/unmute?api_key=${DD_API_KEY}&application_key=${$DD_APP_KEY}"
+    fi
+}
+
+dd_mute
+
 eb deploy $1 -v
 
 if [ $? -eq 0 ]; then
@@ -19,5 +35,7 @@ else
 fi
 
 curl -X POST --data-urlencode 'payload={"username": "Elastic Beanstalk ('"$CIRCLE_PROJECT_REPONAME"')", "icon_url": "'"$SL_ICON"'", "channel": "'"${CHANNEL:-#test}"'", "attachments": [{ "color": "'"$SL_COLOR"'", "text": "'"$SL_TEXT"'", "mrkdwn_in": ["text"] }] }' https://hooks.slack.com/services/${SLACK_HOOK}
+
+dd_unmute
 
 exit $EXIT
