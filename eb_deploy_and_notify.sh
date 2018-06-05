bfeba0a6ebfaf705b187fc9a28cadc9dc5a58f62
hirocaster <hohtsuka@gmail.com>
2018-06-05 22:09:00 +0900

    Only notice by rollbar at success
     - Remove direct notice for slack at success

--- eb_deploy_and_notify.sh
+++ eb_deploy_and_notify.sh
@@ -30,21 +30,18 @@ dd_mute
 eb deploy $1 -v --timeout 15
 
 if [ $? -eq 0 ]; then
-    export SL_COLOR="good"
-    export SL_TEXT="Success: Deployed ${CIRCLE_BRANCH} (<${CIRCLE_COMPARE_URL}|${SHA1}>) by ${CIRCLE_USERNAME} !!"
-    export SL_ICON="https://www.cloudbees.com/sites/default/files/eleasticbeanstalk_square.png"
-    export EXIT=0
     notify
 else
     export SL_COLOR="danger"
     export SL_TEXT="Failure: Deploying ${CIRCLE_BRANCH} (<${CIRCLE_COMPARE_URL}|${SHA1}>) by ${CIRCLE_USERNAME} !!"
     export SL_ICON="https://www.cloudbees.com/sites/default/files/eleasticbeanstalk_square.png"
     export EXIT=1
+
+    if [[ ${SLACK_HOOK+x} ]]; then
+       curl -X POST --data-urlencode 'payload={"username": "Elastic Beanstalk ('"$CIRCLE_PROJECT_REPONAME"')", "icon_url": "'"$SL_ICON"'", "channel": "'"${CHANNEL:-#test}"'", "attachments": [{ "color": "'"$SL_COLOR"'", "text": "'"$SL_TEXT"'", "mrkdwn_in": ["text"] }] }' https://hooks.slack.com/services/${SLACK_HOOK}
+    fi
 fi
 
-if [[ ${SLACK_HOOK+x} ]]; then
-    curl -X POST --data-urlencode 'payload={"username": "Elastic Beanstalk ('"$CIRCLE_PROJECT_REPONAME"')", "icon_url": "'"$SL_ICON"'", "channel": "'"${CHANNEL:-#test}"'", "attachments": [{ "color": "'"$SL_COLOR"'", "text": "'"$SL_TEXT"'", "mrkdwn_in": ["text"] }] }' https://hooks.slack.com/services/${SLACK_HOOK}
-fi
 
 dd_unmute
 
